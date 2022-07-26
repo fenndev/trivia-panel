54588902595712a73545b8f6118e8d7f6a9825fa
fenndev <fenndev@protonmail.com>
2022-07-25 22:04:51 -0700

    Created basic functions for working with categorys

--- src/main.js
+++ src/main.js
@@ -32,3 +32,63 @@ class Category {
         return this.songArray[index];
     }
 }
+
+
+class CategoryManager {
+    categoryFolder = path.join(__dirname, 'categories');
+    currentCategory;
+
+    constructor() {
+
+    }
+
+    async initialize() {
+        await findAllCategories();
+    }
+
+    async findAllCategories() {
+        let categoryJSONFile = path.resolve(this.categoryFolder, 'categories.json');
+        let categories = await fs.readJSON(categoryJSONFile);
+
+
+
+    }
+
+    setCurrentCategory(category) {
+        this.currentCategory = category;
+    }
+
+    getCurrentCategory() {
+        return this.currentCategory;
+    }
+};
+
+// Category Creation
+
+async function createCategory(categoryName) {
+    let category = new Category(categoryName);
+    
+}
+
+function createCategory(categoryName) {
+    let category = new Category(categoryName);
+    return category;
+}
+
+// Save Category
+
+function saveCategory(category) {
+    let categoryJSON = JSON.stringify(category);
+    fs.writeFileSync(path.join(__dirname, 'categories', category.categoryName + '.json'), categoryJSON);
+}
+
+function loadCategories() {
+    let categories = [];
+    let categoryFiles = fs.readdirSync(path.join(__dirname, 'categories'));
+    for(const category in categoryFiles) {
+        let categoryJSON = fs.readFileSync(path.join(__dirname, 'categories', categoryFiles[category]));
+        let categoryObject = JSON.parse(categoryJSON);
+        categories.push(categoryObject);
+    }
+    return categories;
+}
