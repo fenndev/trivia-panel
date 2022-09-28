4963d19f2c88d1e2ab8e3276dee63ca9bf1515a1
fenndev <fenndev@protonmail.com>
2022-09-28 05:07:12 -0700

    Removed unncessary files and folders

---
+++ src/interfaces/question.ts
@@ -0,0 +1,10 @@
+interface Question {
+    gameName: string;
+    imagePath: string;
+    songName: string;
+    songPath: string;
+    pointValue: number;
+    questionID: string;
+};
+
+export { Question };
