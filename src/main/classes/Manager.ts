de5c4b2839f66033d4e69d88adaf87480f012a1f
fenndev <fenndev@proton.me>
2023-06-20 16:36:01 -0700

    Removed unused code & made Manager const. private

--- src/main/classes/Manager.ts
+++ src/main/classes/Manager.ts
@@ -1,4 +1,4 @@
-import { Song, RawSong, ParsedSong } from '../../shared/interfaces/Song';
+import { RawSong, ParsedSong } from '../../shared/interfaces/Song';
 import Collection from '../../shared/classes/Collection';
 import FileManager from './FileManager';
 import Category from '../../shared/interfaces/Category';
@@ -8,7 +8,7 @@ class Manager {
     private static _instance: Manager;
     private _collection: Collection;
     private _fileManager: FileManager;
-    constructor() {
+    private constructor() {
         this._fileManager = new FileManager();
         this._collection = new Collection();
     }
@@ -26,31 +26,16 @@ class Manager {
         const parsedSong: ParsedSong = toParsedSong(song, imageFilePath, songFilePath);
         this._collection.addSong(parsedSong, categoryID);
         this._fileManager.syncJSON(this._collection.toJSON());
-        // if (this._songManager.isRawSong(song) && this._categoryManager.categoryExists(song.categoryID)) {
-        //     const category: Category = this._categoryManager.getCategory(song.categoryID) as Category;
-        //
-        //     const parsedSong: ParsedSong = parseSong(song, imageFilePath, songFilePath);
-        //     this._songManager.addSong(parsedSong);
-        //     this._categoryManager.updateCategory(category);
-        //     this._fileManager.syncJSON(this._categoryManager.categories);
-        // } else return;
     }
 
     public onNewCategory(category: Category): void {
         this._collection.addCategory(category);
         this._fileManager.syncJSON(this._collection.toJSON());
-        // if (this._categoryManager.categoryExists(category.id)) return;
-        // this._categoryManager.addCategory(category);
-        // this._fileManager.syncJSON(this._categoryManager.categories);
     }
 
     get collection(): Collection {
         return this._collection;
     }
-
-    // public findCategory = (categoryID: string): Category | undefined => this._categoryManager.getCategory(categoryID);
-
-    // public findSong = (songID: string): Song | undefined => this._songManager.getSong(songID);
 }
 const manager = Manager.getInstance();
 
